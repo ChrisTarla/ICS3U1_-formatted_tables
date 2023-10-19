ace0a67f79998e74e8b1a1e7c4eaf9ef12f437b8
ChrisTarla <christine.tarla26@ycdsbk12.ca>
2023-10-19 09:29:37 -0400

    formatted strings

--- KmTOMiles2.java
+++ KmTOMiles2.java
@@ -1,22 +1,26 @@
-class Program1 extends ConsoleProgram {
+class KmTOMiles2 extends ConsoleProgram {
 
 /**
   * Description
   * A program that converts kilometers to miles from 10 to 100km, every 10km in a formatted table
   * @author: E. Fabroa
   */
-
   
   public void run() {
 
-     double dblMiles;
+    double dblMiles;
 
+    // Output the table header 
+    System.out.printf("%9S %12S %n", "Km", "Miles");
+    System.out.println("______________________");
 
+    // Output the table body 
     for (int intKm = 10; intKm <= 100; intKm += 10){
       dblMiles = intKm * 0.621371;
-      System.out.println(intKm + "km --> " + dblMiles + "mi");
+      //System.out.println(intKm + "km --> " + dblMiles + "mi");
 
-   
-    
+      // Output the row in formatted strings
+      System.out.printf("%7dkm %10.2fmi %n", intKm, dblMiles);
+    }
   }
 }
